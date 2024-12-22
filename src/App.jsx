7b85e6494f6a36ff74bc2c2718e049888b408447
Prannat <prannat@dal.ca>
2024-12-22 18:29:49 -0400

    experience import fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Bio from "./components/bio/Bio";
-import Experience from "./components/experience/experience";
 
 const App = () => {
   return (
