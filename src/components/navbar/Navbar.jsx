94e75c19c4e3336d4df44f964aa1d85ba458700f
Prannat Jain <prannatj@gmail.com>
2024-12-25 15:58:38 -0400

    mobile css formatting

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import "./navbar.scss";
-import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
